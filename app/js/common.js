1c7b92d0585cdbeaf61965cfcdcbbc2bf1f6622d
SenkoTaras <taras.senko.99@gmail.com>
2019-05-28 16:19:03 +0300

    v 0.0.7
    007

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,7 @@ $(function() { var logo = $(".section_header");
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 500) {
+    if (scroll >= 500 && $(window).width() > 1200) {
         if(!logo.hasClass("section_header__small")) {
             logo.hide();
             logo.removeClass('section_header').addClass("section_header__small").fadeIn( "slow");
@@ -17,7 +17,23 @@ $(window).scroll(function() {
 });
 });
 
-$('.burger, .overlay').click(function(){
+$('.burger, .header_menu').click(function(){
     $('.burger').toggleClass('burger_active');
+    $('.header_menu').toggleClass('header_menu_active');
 
+});
+
+$('.lang_item_active').click(function(){
+    $('.lang_wrapper').toggleClass('lang_wrapper_active');
+    $('.lang_item').toggleClass('lang_item_open');
+});
+
+$(function () {
+    $('.menu_links').each(function () {
+        var location = window.location.href;
+        var link = this.href;
+        if(location == link) {
+            $(this).addClass('menu_links_active');
+        }
+    });
 });
